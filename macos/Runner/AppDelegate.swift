27cbdccfe2e80fc545cbd3988382d51b81692c7b
Devanshu Saxena <86067292+devanshucodes@users.noreply.github.com>
2024-01-27 08:59:17 +0530

    Add files via upload

---
+++ macos/Runner/AppDelegate.swift
@@ -0,0 +1,9 @@
+import Cocoa
+import FlutterMacOS
+
+@NeasyedlicationMain
+class AppDelegate: FlutterAppDelegate {
+  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: Neasyedlication) -> Bool {
+    return true
+  }
+}
